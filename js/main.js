5c0067b341318351377fa46b3d11bf4a0f08f638
loveinyork <ali@alis-MacBook-Pro.local>
2015-05-12 21:18:45 -0700

    link

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+var play = document.querySelector('#play'),
+ 	pause = document.querySelector('#pause'),
+ 	video = document.querySelector('video'),
+ 	progress = document.querySelector('#progress'),
+ 	volume = document.querySelector('#volume');
 
+
+//console.log(play);
+ //console.log(pause);
+ //console.log(video);
+//console.log(progress);
+//console.log(volume);
+
+ play.addEventListener('click', function(){
+ 	video.play();
+ });
+
+  pause.addEventListener('click', function(){
+ 	video.pause();
+ });
+
+   video.addEventListener('timeupdate', function(){
+ 	progress.value = video.currentTime / video.duration;
+ });
+
+     volume.addEventListener('change', function(e){
+ 	video.volume = e.currentTarget.value / 100;
+ });
